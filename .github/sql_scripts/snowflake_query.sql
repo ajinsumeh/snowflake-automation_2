0b19e93dd33f25483df9561873de7f2cc5f07e33
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-10-28 20:22:53 +0530

    Update snowflake_query.sql

--- .github/sql_scripts/snowflake_query.sql
+++ .github/sql_scripts/snowflake_query.sql
@@ -1,4 +1,4 @@
 insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (3,'Ajin3','003');
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (7,'Ajin4','004');
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (8,'Ajin4','004');
 
 
