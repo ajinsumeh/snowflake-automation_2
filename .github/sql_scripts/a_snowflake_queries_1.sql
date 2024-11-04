e542ab57ec23d0c01820823e2d8b6a13c8d47d9e
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-11-04 19:18:44 +0530

    Delete .github/sql_scripts/a_snowflake_queries_1.sql

--- .github/sql_scripts/a_snowflake_queries_1.sql
+++
@@ -1,5 +0,0 @@
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (2,'Ajin','002');
-
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE1 values (3,'Ajin','003');
-
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (4,'Ajin','004');
