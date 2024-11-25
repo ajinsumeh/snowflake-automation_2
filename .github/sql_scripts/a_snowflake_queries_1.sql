afa17e4edf19a324b5ef4808074edac8adde6da3
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-11-25 21:04:54 +0530

    Add files via upload

---
+++ .github/sql_scripts/a_snowflake_queries_1.sql
@@ -0,0 +1,7 @@
+--hello I am going to automate github snowflake automation
+
+-- This is data row 1
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (1,'Ajin');
+
+-- This is data row 2
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (2,'Arya');
