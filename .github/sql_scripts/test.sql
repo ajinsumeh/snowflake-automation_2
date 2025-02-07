7a4906cc5e3ef8c9683867b3914c1fb8200c9275
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2025-02-07 19:09:35 +0530

    Delete .github/sql_scripts/test.sql

--- .github/sql_scripts/test.sql
+++
@@ -1,8 +0,0 @@
-alter table TEST_DATABASE.GIT_FETCH.EMPLOYEE ADD COLUMN (phone_number int);
-
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (1, 'Ajin');
-
-alter table TEST_DATABASE.GIT_FETCH.EMPLOYEE1 ADD COLUMN (phone_number int);
-
-
-
