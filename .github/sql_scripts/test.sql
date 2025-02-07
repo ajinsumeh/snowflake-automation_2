0ea44b31cfcb01f18240a196b0327bc9d32bd2b5
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2025-02-07 19:14:28 +0530

    Add files via upload

---
+++ .github/sql_scripts/test.sql
@@ -0,0 +1,8 @@
+alter table TEST_DATABASE.GIT_FETCH.EMPLOYEE ADD COLUMN phone_number varchar(20);
+
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (1, 'Ajin');
+
+alter table TEST_DATABASE.GIT_FETCH.EMPLOYEE1 ADD COLUMN phone_number varchar(10);
+
+
+
