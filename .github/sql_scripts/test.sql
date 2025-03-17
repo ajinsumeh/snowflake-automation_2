d811a923202b4f9d9104afa6f56b1faf04f54eca
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2025-03-17 09:31:44 +0530

    Update test.sql

--- .github/sql_scripts/test.sql
+++ .github/sql_scripts/test.sql
@@ -1,8 +1,6 @@
-alter table TEST_DATABASE.GIT_FETCH.EMPLOYEE ADD COLUMN phone_number varchar(20);
+insert into GITHUB_AUTOMATION.GIT.GIT_TABLE(2,'Ajin','Y');
 
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values(1, 'Ajin');
 
-alter table TEST_DATABASE.GIT_FETCH.EMPLOYEE1 ADD COLUMN phone_number varchar(10);
 
 
 
